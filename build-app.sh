ce90e2e6b9ca9a407c575158bc873d889abf9427
TheShovel <niccata24@gmail.com>
2024-08-09 11:24:19 +0300

    more logging

--- build-app.sh
+++ build-app.sh
@@ -6,24 +6,27 @@ rm -rf windows-base
 rm -rf penguinmod-linux.zip
 rm -rf penguinmod-windows.zip
 
+echo "Downloading Electron - Linux"
 #Getting the electron package from the PenguinMod servers
 #Linux
 curl --output linux.zip https://packagerdata.turbowarp.org/electron-v22.3.27-linux-x64.zip
 unzip linux.zip -d linux-base
 rm -rf linux.zip
 rm -rf linux-base/resources/default_app.asar
-
+echo "Downloading Electron - Windows"
 #Windows
 curl --output windows.zip https://packagerdata.turbowarp.org/electron-v22.3.27-win32-ia32.zip
 unzip windows.zip -d windows-base
 rm -rf windows.zip
 rm -rf windows-base/resources/default_app.asar
 
+echo "Downloading PenguinMod"
 export NODE_OPTIONS=--openssl-legacy-provider
 git clone https://github.com/PenguinMod/penguinmod.github.io.git
 cd penguinmod.github.io
 git pull
 npm install --force
+echo "Building PenguinMod"
 npm run --silent build
 sleep 5s
 cp -R build ../app
